898a77a574723c1ab17ce8c3ee3fc9306ac01d53
selimreza777 <selimrezacse@gmail.com>
2025-08-02 02:57:37 +0600

    for sum

---
+++ for-sum.js
@@ -0,0 +1,16 @@
+// let sum = 0;
+// for (let i = 11; i <= 20; i++) {
+//   sum = sum + i;
+//   console.log(i)
+// }
+// console.log('sum 11 to 20 number added', sum)
+
+let sum = 0;
+let i = 11;
+while (i <= 20) {
+  sum = sum + i;
+  console.log(i)
+
+  console.log('sum 11 to 20 number added', sum)
+  i++
+}
