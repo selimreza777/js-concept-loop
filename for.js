f5ebacc280b05dfe75697876f752bcc72740480d
selimreza777 <selimrezacse@gmail.com>
2025-08-02 02:37:45 +0600

    odd even

---
+++ for.js
@@ -0,0 +1,31 @@
+
+/*
+for (   ;    ;      ){
+}
+
+
+for(first part;  second part;   third part){
+
+loop statement
+}
+
+for(loop variable declaration ; loop condition  ; loop variable change)
+
+
+
+
+*/
+
+
+// let num = 0;   //loop variable
+// while (num <= 10) {    //loop condition
+//   console.log(num)     // loop statement
+//   num++;               // loop variable change
+// }
+
+
+// for(loop variable declaration ; loop condition  ; loop variable change)
+
+for (let num = 0; num < 5; num++) {   
+  console.log(num)
+}
---
+++ odd-even.js
@@ -0,0 +1,25 @@
+// for (let num = 1; num <= 10; num++) {
+//   console.log(num)
+// }
+
+// for (let i = 1; i <= 50; i++){
+//   console.log(i)
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 === 0) {
+
+
+//     console.log(`Even Number ${i}:even`)
+//   }
+// }
+
+// even 
+for (let i = 0; i <= 20; i = i + 2) {
+  console.log(`evern ${i}`)
+}
+
+// odd 
+for (let i = 1; i <= 20; i = i + 2) {
+  console.log(`odd ${i}`)
+}
