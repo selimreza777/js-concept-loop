79b459ad8cc9685b0a7a23579566d518227bddd6
selimreza777 <selimrezacse@gmail.com>
2025-08-02 01:34:28 +0600

    even commit

---
+++ even.js
@@ -0,0 +1,14 @@
+// let num = 1;
+// while (num <= 10) {
+//   console.log(num);
+//   if (num % 2 === 0) {
+//     console.log('even:', num)
+//   }
+//   num++;
+// }
+
+let i = 1;
+while (i <= 60) {
+  console.log(`Day ${i}: I will invest 6 hours every day in 60 days`);
+  i++;
+}
---
+++ range.js
@@ -0,0 +1,13 @@
+// let num = 2;
+//   while (num < 10) {   //foucse on the condition
+//   // console.log(num)
+//   num++;
+//   console.log(num)
+// }
+
+let num = 50;
+
+while (num <= 100) {
+  console.log(num)
+  num++;
+}
