9b2a28fcbb3187a2ee6dcb52122913c456ca0773
selimreza777 <selimrezacse@gmail.com>
2025-08-01 18:20:08 +0600

    recap.js create

---
+++ recap.js
@@ -0,0 +1,17 @@
+// const : value will not change 
+// let : value can change 
+// var : we should not use it 
+
+//varaiable
+const amount = 200;
+
+// condition 
+if (amount > 100){
+
+}
+else{
+
+}
+
+// array 
+const number =[10, 20, 40, 47, 89, 78];
