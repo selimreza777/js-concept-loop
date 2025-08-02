021008e4acc5228db12616a33c599369417d7753
selimreza777 <selimrezacse@gmail.com>
2025-08-02 14:18:45 +0600

    decrement

---
+++ decremental-for.js
@@ -0,0 +1,17 @@
+// incremental loop 
+
+// for (i = 0; i <= 10; i++) {
+//   console.log(`inrementaol`, i)
+// }
+
+// // decremental for loop 
+// for (let i = 10; i >= 0; i--) {
+//   console.log(`decremental`, i)
+// }
+
+// while loop use decremental number 
+let n = 10;
+while (n >= 0) {
+  console.log(`decrement`, n)
+  n--;
+}
