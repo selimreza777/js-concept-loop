906112d17de1b44fb344ec7b1c013aebb4d6d53c
selimreza777 <selimrezacse@gmail.com>
2025-08-02 16:57:09 +0600

    break

---
+++ break.js
@@ -0,0 +1,21 @@
+// for (i = 0; i < 15; i++) {
+//   console.log(i);
+//   if (i >= 8) {
+//     break;
+//   }
+// }
+// console.log(`life after breakup`);
+
+let n = 54;
+while (n > 25) {
+  console.log(n);
+  if (n < 50) {
+
+    break;
+
+
+  }
+  n--;
+
+
+}
